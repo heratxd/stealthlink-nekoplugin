049dbfed5aa501049b0c37ef0d4cffd285708721
HeratXD <nikolai2011garkusha@gmail.com>
2026-05-28 02:01:43 +0300

    fix: remove plugin versions from root build script to resolve buildSrc conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("com.android.library") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.android.application") apply false
+    id("com.android.library") apply false
+    id("org.jetbrains.kotlin.android") apply false
 }
